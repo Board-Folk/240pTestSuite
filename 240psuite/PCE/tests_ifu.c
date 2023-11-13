06f98bac05c0903eb06794a1152265ebec1b8ae2
Cosam <git@cosam.org>
2023-11-13 19:25:23 +0100

    Basic backup RAM testing.

--- 240psuite/PCE/tests_ifu.c
+++ 240psuite/PCE/tests_ifu.c
@@ -37,6 +37,228 @@ extern unsigned char EnabledSoft;
 extern unsigned char Enabled_C_BW;
 #endif
 
+#define FONT_WHITE 14
+#define FONT_RED   15
+#define FONT_GREEN 13
+#define FONT_GREY  12
+
+#define IFU_REG_MSB 0x18
+
+#define IFU_REGS 15
+const char *ifu_reg_names[IFU_REGS] = {
+  "CDC Status",
+  "CDC Command",
+  "ADPCM/CD Control",
+  "BRAM Lock",
+  "CD Reset",
+  "Convert PCM Data",
+  "PCM Data",
+  "BRAM Unlock",
+  "ADPCM Addr (LSB)",
+  "ADPCM Addr (MSB)",
+  "ADPCM RAM Data",
+  "ADPCM DMA Control",
+  "ADPCM Status",
+  "ADPCM Addr Control",
+  "Audio Fade Timer"
+};
+
+#define RAM_TEST_VALUES 4
+const char ram_test_values[RAM_TEST_VALUES] = {
+  0x00,
+  0x55,
+  0xAA,
+  0xFF
+};
+
+
+void BackupRAMTest()
+{
+  int  i = 0;
+  char *addr;
+  int  bram_size = -1;
+  int  bram_free = 0;
+  int  full_test = 0;
+  int  full_test_pass = 0;
+  int  full_offset = 0;
+  char full_wrote = 0;
+  char full_read = 0;
+  char full_temp = 0;
+
+  redraw = 1;
+
+  while(!end)
+  {
+    vsync();
+
+    if(redraw)
+    {
+      RedrawBG();
+      refresh = 1;
+      redraw = 0;
+      disp_sync_on();
+    }
+    if(refresh)
+    {
+      SetFontColors(FONT_WHITE, RGB(3, 3, 3), RGB(7, 7, 7), 0);
+      SetFontColors(FONT_RED,   RGB(3, 3, 3), RGB(7, 0, 0), 0);
+      SetFontColors(FONT_GREEN, RGB(3, 3, 3), RGB(0, 7, 0), 0);
+      SetFontColors(FONT_GREY,  RGB(3, 3, 3), RGB(5, 5, 5), 0);
+
+      set_font_pal(FONT_GREEN);
+      put_string("IFU: Backup RAM", 12, 7);
+
+      set_font_pal(FONT_WHITE);
+      put_string("Detected:", 12, 10);
+      if ( bm_check() ) {
+        bram_size = bm_size();
+        bram_free = bm_free();
+
+        set_font_pal(FONT_GREEN);
+        put_string("YES", 25, 10);
+
+        set_font_pal(FONT_WHITE);
+        put_string("Total Bytes:", 9, 11);
+        put_number(bram_size, 8, 21, 11);
+
+        put_string("Bytes Free:", 10, 12);
+        put_number(bram_free, 8, 21, 12);
+      }
+      else {
+        set_font_pal(FONT_RED);
+        put_string("NO", 25, 10);
+      }
+
+      if( full_test ) {
+        set_font_pal(FONT_WHITE);
+        put_string("Full BRAM test:", 6, 14);
+        put_string("Press RUN to test again", 8, 22);
+
+        if ( full_test_pass ) {
+          set_font_pal(FONT_GREEN);
+          put_string("PASSED", 25, 14);
+        }
+        else {
+          set_font_pal(FONT_RED);
+          put_string("FAILED", 25, 14);
+
+          set_font_pal(FONT_GREY);
+          put_string("At offset: $xxxx", 12, 16);
+          put_string("Wrote: $xx  Read: $xx", 9, 17);
+          put_string("Original value: $xx", 10, 18);
+          put_hex(full_offset, 4, 24, 16);
+          put_hex(full_wrote,  2, 17, 17);
+          put_hex(full_read,   2, 28, 17);
+          put_hex(full_temp,   2, 27, 18);
+
+          if( full_read == full_temp ) {
+            set_font_pal(FONT_RED);
+            put_string("BRAM unlock error?", 11, 19)
+          }
+        }
+      }
+      else {
+        set_font_pal(FONT_WHITE);
+        put_string("Press RUN for full test", 8, 14);
+        set_font_pal(FONT_RED);
+        put_string("Contents may be destroyed!", 6, 16);
+      }
+
+      set_font_pal(FONT_WHITE);
+      put_string("Press II to exit", 11, 24);
+    }
+
+    controller = joytrg(0);
+
+    if ( controller & JOY_RUN ) {
+      full_test = 1;
+      full_test_pass = 1;
+
+      if( bram_size < 0 ) {
+        // Backup RAM not detected, but assume 2K and allow to test.
+        bram_size = 2048;
+      }
+
+      for( full_offset = 0 ; full_offset < bram_size ; full_offset++ ) {
+        for( i = 0 ; i < RAM_TEST_VALUES ; i++ ) {
+          full_temp = bm_rawread(full_offset);
+
+          full_wrote = ram_test_values[i];
+          bm_rawwrite(full_offset, full_wrote);
+          full_read = bm_rawread(full_offset);
+
+          // Restore "original" contents (assuming correctly read...)
+          bm_rawwrite(full_offset, full_temp);
+
+          if (full_read != full_wrote) {
+            bm_disable(); // Insurance?
+            full_test_pass = 0;
+            break;
+          }
+        }
+        if( full_test_pass == 0 ) {
+          break;
+        }
+      }
+
+      redraw = 1;
+    }
+    else if ( controller & JOY_II ) {
+      end = 1;
+    }
+
+  }
+}
+
+
+void IFURegisterTest()
+{
+  int i = 0;
+  char row = 0;
+  char *addr;
+  redraw = 1;
+
+  while(!end)
+  {
+    vsync();
+
+    if(redraw)
+    {
+      RedrawBG();
+      refresh = 1;
+      redraw = 0;
+      disp_sync_on();
+    }
+    if(refresh)
+    {
+      SetFontColors(FONT_WHITE, RGB(3, 3, 3), RGB(7, 7, 7), 0);
+      SetFontColors(FONT_RED,   RGB(3, 3, 3), RGB(7, 0, 0), 0);
+      SetFontColors(FONT_GREEN, RGB(3, 3, 3), RGB(0, 7, 0), 0);
+      SetFontColors(FONT_GREY,  RGB(3, 3, 3), RGB(5, 5, 5), 0);
+
+      set_font_pal(FONT_GREEN);
+      put_string("IFU Registers", 13, 6);
+
+      set_font_pal(FONT_WHITE);
+      for (i = 0 ; i < IFU_REGS ; i++ ) {
+        row = i+8;
+        put_string("$", 4, row);
+        put_hex(IFU_REG_MSB, 2, 5, row);
+        put_hex(i, 2, 7, row);
+        put_string(ifu_reg_names[i], 10, row);
+
+        addr = (IFU_REG_MSB << 8) | (i & 0xff);
+        put_hex(*addr, 2, 34, row);
+      }
+    }
+
+    if( joytrg(0) != 0 ) {
+      end = 1;
+    }
+
+  }
+}
+
 void RefreshIFUTests()
 {
   set_font_pal(12);
@@ -144,13 +366,13 @@ void IFUTests()
       switch(sel)
       {
         case 0:
-          //  IFU Registers
+          IFURegisterTest();
           break;
         case 1:
           // CD System RAM
           break;
         case 2:
-          // Backup RAM
+          BackupRAMTest();
           break;
         case 3:
           // ADPCM
